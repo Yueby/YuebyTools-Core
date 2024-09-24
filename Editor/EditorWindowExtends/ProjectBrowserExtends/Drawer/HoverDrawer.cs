041b38fad9eff5b54eb14945c1adba0d31a1cad5
月白 <yueby_sp@qq.com>
2024-09-25 02:07:36 +0800

    Update HoverDrawer.cs

--- Editor/EditorWindowExtends/ProjectBrowserExtends/Drawer/HoverDrawer.cs
+++ Editor/EditorWindowExtends/ProjectBrowserExtends/Drawer/HoverDrawer.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 using Yueby.EditorWindowExtends.ProjectBrowserExtends.Core;
 using Yueby.EditorWindowExtends.Reflections;
 using Yueby.EditorWindowExtends.Utils;
+using YuebyTools.Core.Utils;
 
 namespace Yueby.EditorWindowExtends.ProjectBrowserExtends.Drawer
 {
     public class HoverDrawer : ProjectBrowserDrawer
     {
         public override string DrawerName => "Hover Background";
+        private Rect _hoverRect;
+        private bool _isDown;
+        private AssetItem _lastHoverItem;
 
         public override void OnProjectBrowserObjectAreaItemGUI(AssetItem item)
         {
             DrawHover(item);
         }
 
-        public override void OnProjectBrowserTreeViewItemGUI(AssetItem item, TreeViewItem treeViewItem)
+        public override void OnProjectBrowserTreeViewItemGUI(
+            AssetItem item,
+            TreeViewItem treeViewItem
+        )
         {
             DrawHover(item);
         }
@@ -36,31 +43,49 @@ namespace Yueby.EditorWindowExtends.ProjectBrowserExtends.Drawer
 
             if (item.OriginRect.height > EditorGUIUtility.singleLineHeight)
             {
-                const float expand = 2;
-                rect.x -= expand;
-                rect.width += expand * 2;
-                rect.y -= expand;
-                rect.height += expand * 2;
+                rect = ExpandRect(rect, 2);
             }
 
-            // Debug.Log("rect: " + rect);
-            if (!item.IsHover /*|| Selection.activeObject == item.Asset*/) return;
+            if (!item.IsHover)
+                return;
+
+            if (_lastHoverItem != item)
+            {
+                _lastHoverItem = item;
+                _isDown = false;
+            }
+
+            switch (Event.current.type)
+            {
+                case EventType.MouseDown:
+                    _isDown = true;
+                    break;
+                case EventType.MouseUp:
+                    _isDown = false;
+                    break;
+            }
+
+            rect = ExpandRect(rect, _isDown ? 4 : 2);
 
             var color = GameObjectStylesReflect.GetHoveredBackgroundColor();
 
             using (new BackgroundColorScope(color))
             {
                 var style = GameObjectStylesReflect.GetHoveredItemBackgroundStyle();
-                // if (item.OriginRect.height > EditorGUIUtility.singleLineHeight)
-                // {
-                //     style.normal.background = EditorGUIUtility.whiteTexture;
-                // }
-                GUI.Label(
-                    rect,
-                    GUIContent.none,
-                    style
-                );
+                GUI.Label(rect, GUIContent.none, style);
             }
+
+            // Extender.Repaint();
+        }
+
+        private Rect ExpandRect(Rect rect, int expand)
+        {
+            return new Rect(
+                rect.x - expand,
+                rect.y - expand,
+                rect.width + expand * 2,
+                rect.height + expand * 2
+            );
         }
     }
 }
