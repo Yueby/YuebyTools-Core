c34dadc21139f72ee71dadcb9e8aaccec0cf0c01
月白 <yueby_sp@qq.com>
2024-11-12 20:07:28 +0800

    1

--- Editor/Utils/Reflections/ReflectionUtil.cs
+++ Editor/Utils/Reflections/ReflectionUtil.cs
@@ -5,6 +5,9 @@ namespace Yueby.Utils.Reflections
 {
     public static class ReflectionUtil
     {
-        public static object GetFieldValue(this Type type, string fieldName, object obj) => type.Field(fieldName).GetValue(obj);
+        public static object GetFieldValue(this Type type, string fieldName, object obj)
+        {
+            return type.GetField(fieldName).GetValue(obj);
+        }
     }
 }
