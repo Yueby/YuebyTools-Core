483fcd3bc3a6e9f6136c23905f85be183130c8b0
月白 <yueby_sp@qq.com>
2024-11-13 18:34:03 +0800

    Update ReflectionUtil.cs

--- Editor/Utils/Reflections/ReflectionUtil.cs
+++ Editor/Utils/Reflections/ReflectionUtil.cs
@@ -1,13 +1,65 @@
 using System;
+using System.Reflection;
 using HarmonyLib;
 
 namespace Yueby.Utils.Reflections
 {
     public static class ReflectionUtil
     {
-        public static object GetFieldValue(this Type type, string fieldName, object obj)
+        public static readonly BindingFlags all =
+            BindingFlags.GetField
+            | BindingFlags.GetProperty
+            | BindingFlags.Instance
+            | BindingFlags.NonPublic
+            | BindingFlags.Public
+            | BindingFlags.SetField
+            | BindingFlags.SetProperty
+            | BindingFlags.Static;
+
+        public static readonly BindingFlags allDeclared = all | BindingFlags.DeclaredOnly;
+
+        public static object GetFieldValue(this Type type, string fieldName, object obj) =>
+            Field(type, fieldName).GetValue(obj);
+
+        public static FieldInfo Field(Type type, string name)
         {
-            return type.GetField(fieldName).GetValue(obj);
+            if ((object)type == null)
+            {
+                FileLog.Debug("ReflectionUtil.Field: type is null");
+                return null;
+            }
+
+            if (name == null)
+            {
+                FileLog.Debug("ReflectionUtil.Field: name is null");
+                return null;
+            }
+
+            FieldInfo fieldInfo = FindIncludingBaseTypes(type, (Type t) => t.GetField(name, all));
+            if ((object)fieldInfo == null)
+            {
+                FileLog.Debug(
+                    $"ReflectionUtil.Field: Could not find field for type {type} and name {name}"
+                );
+            }
+
+            return fieldInfo;
+        }
+
+        public static T FindIncludingBaseTypes<T>(Type type, Func<Type, T> func)
+            where T : class
+        {
+            do
+            {
+                T val = func(type);
+                if (val != null)
+                {
+                    return val;
+                }
+
+                type = type.BaseType;
+            } while ((object)type != null);
+            return null;
         }
     }
 }
